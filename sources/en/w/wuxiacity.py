6902624b20c6b1b30991a2589b0f1936658dd3c6
Sudipto Chandra <dipu.sudipta@gmail.com>
2026-04-01 21:15:05 +0400

    Refactor Crawler classes to use LegacyCrawler
    - Updated multiple Crawler classes to inherit from LegacyCrawler for improved functionality and consistency across the codebase.
    - Adjusted import statements to reflect the new structure, enhancing code clarity and maintainability.
    - Ensured that all affected classes maintain their original functionality while aligning with the new architecture.

--- sources/en/w/wuxiacity.py
+++ sources/en/w/wuxiacity.py
@@ -3,15 +3,14 @@
 import logging
 from urllib.parse import quote_plus
 
-from lncrawl.core import Crawler
-from lncrawl.models import Chapter, Volume
+from lncrawl.core import Chapter, LegacyCrawler, Volume
 
 logger = logging.getLogger(__name__)
 
 search_url = "https://www.wuxia.city/search?q=%s"
 
 
-class WuxiaCityCrawler(Crawler):
+class WuxiaCityCrawler(LegacyCrawler):
     base_url = [
         "https://wuxia.city",
     ]
